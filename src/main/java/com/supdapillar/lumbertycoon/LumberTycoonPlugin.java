1a7cb6da8dc11c70c1f788548e75acc8c25addc9
Supdapillar <Supdapillar@gmail.com>
2023-08-07 23:25:05 -0400

    Start of project

---
+++ src/main/java/com/supdapillar/lumbertycoon/LumberTycoonPlugin.java
@@ -0,0 +1,17 @@
+package com.supdapillar.lumbertycoon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LumberTycoonPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
